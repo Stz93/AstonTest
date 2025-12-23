eb45d05c13ace9a68be4658089db061269ebfd7e
Bogdan <rabotakuvaldoi@gmail.com>
2025-12-23 20:21:28 +0400

    message

---
+++ src/main/java/org/example/FileInputStrategy.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class FileInputStrategy implements InputStrategy {
+    String filePath;
+    public FileInputStrategy(String filePath) {
+        this.filePath = filePath;
+    }
+    @Override
+    public CustomArrayList<Student> loadData()
+    {
+        try {
+            return Files.lines(Paths.get(filePath))
+                    .map(this::parseLine)
+                    .collect(Collectors.toCollection(CustomArrayList::new));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Student parseLine(String line)
+    {
+
+            String[] parts = line.split(",");
+            try{
+            if (parts.length != 3) {
+            } } catch (Exception e) {
+                throw new IllegalArgumentException(e);
+            }
+
+            String name = parts[0].trim();
+            double averageGrade = Double.parseDouble(parts[1].trim());
+            int recordBookNumber = Integer.parseInt(parts[2].trim());
+
+            //попозже добавить валидацию, что имя не пустое и с остальным придумать
+
+            return new Student.Builder()
+                    .name(name)
+                    .averageGrade(averageGrade)
+                    .recordBookNumber(recordBookNumber)
+                    .build();
+    }
+}
+//если тут напишу то можно будет кинуть снова
