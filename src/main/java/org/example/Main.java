b0edd75736bcdd40a96ac27bd305249cde7f8085
HanSolo <cppklon@gmail.com>
2025-12-25 15:06:58 +0600

    [+] Многопоточный подсчёт элементов

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Main {
     /**
      * Количество потоков выделенное для многопоточных операций
      */
-    final static int THREAD_NUMBER = 4;
+    final static int NUM_THREADS = 4;
     /**
      * Флаг для прерывания выполнения приложения
      */
@@ -84,7 +86,7 @@ public class Main {
                 System.out.println("\t\trecordBookNumber : ");
                 String recordBookNumber = scanner.next();
 
-                count(name, averageGrade, recordBookNumber, THREAD_NUMBER);
+                count(name, averageGrade, recordBookNumber, studentList);
                 processRequest(scanner, studentList);
                 return;
             case "0":
@@ -105,11 +107,13 @@ public class Main {
         // реализовать паттерн "Стратегия" по введённому коду стратегии, добавить валидацию этого кода.
         Student.Builder builder = new Student.Builder();
 
-        Student[] studentArray = new Student[4];
+        Student[] studentArray = new Student[6];
         studentArray[0] = builder.name("Sasha").averageGrade(0.97).recordBookNumber(100101).build();
         studentArray[1] = builder.name("Bogdan").averageGrade(0.99).recordBookNumber(888888).build();
         studentArray[2] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414141).build();
         studentArray[3] = builder.name("Tagir").averageGrade(0.98).recordBookNumber(366663).build();
+        studentArray[4] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414144).build();
+        studentArray[5] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414144).build();
 
         // Пример заполнения коллекции из стрима:
         Arrays.stream(studentArray).forEach(studentList::add);
@@ -129,14 +133,51 @@ public class Main {
      * @param name             {@link Student#name} для поиска
      * @param averageGrade     {@link  Student#averageGrade} для поиска
      * @param recordBookNumber {@link Student#recordBookNumber} для поиска
-     * @param threadNumber     выделенное количество потоков
+     * @param studentList      Лист в котором будем искать указанного студента
      */
     static void count(final String name,
                       final String averageGrade,
                       final String recordBookNumber,
-                      final int threadNumber) {
-        // реализовать счётчик
-        System.out.println("Посчитан элемент : " + name + ", " + averageGrade + ", " + recordBookNumber);
+                      final List<Student> studentList) {
+        final Student student;
+        try {
+            student = new Student(name, Double.parseDouble(averageGrade), Integer.parseInt(recordBookNumber));
+        } catch (NumberFormatException e) {
+            System.out.println("Values cant be cast to expected formats");
+            return;
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        int bucketSize = studentList.size() / NUM_THREADS;
+        for (int i = 0; i < NUM_THREADS; i++) {
+            final int leftBound = i * bucketSize;
+            final int rightBound = (i == NUM_THREADS - 1) ? studentList.size() : (i + 1) * bucketSize;
+            Callable<Integer> task = () -> {
+                int localCount = 0;
+                for (int j = leftBound; j < rightBound; j++) {
+                    if (studentList.get(j).equals(student)) {
+                        localCount++;
+                    }
+                }
+                return localCount;
+            };
+            futures.add(executor.submit(task));
+        }
+
+        int totalCount = 0;
+        for (Future<Integer> future : futures) {
+            try {
+                totalCount += future.get(); // Собираем результаты
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
+        System.out.println(totalCount + " elements were found for:");
+        System.out.println(student);
+
     }
 
     /**
