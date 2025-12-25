83c58c3728642e50ad84c1f370c630e9c7c3d911
HanSolo <cppklon@gmail.com>
2025-12-26 03:37:53 +0600

    [+] Многопоточный подсчёт элементов [*] Небольшие правки в меню

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,16 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class Main {
     /**
      * Количество потоков выделенное для многопоточных операций
      */
-    final static int THREAD_NUMBER = 4;
+    final static int NUM_THREADS = 4;
     /**
      * Флаг для прерывания выполнения приложения
      */
@@ -70,10 +72,11 @@ public class Main {
                 System.out.println("\t\t2 averageGrade");
                 System.out.println("\t\t3 recordBookNumber");
                 System.out.println("\t\t4 only even recordBookNumber");
-                System.out.println("\t\t5 back");
+                System.out.println("\t\t0 back");
 
                 String sortCode = scanner.next();
                 sort(sortCode, studentList);
+
                 processRequest(scanner, studentList);
                 return;
             case "4":
@@ -85,11 +88,18 @@ public class Main {
                 System.out.println("\t\trecordBookNumber : ");
                 String recordBookNumber = scanner.next();
 
-                count(name, averageGrade, recordBookNumber, THREAD_NUMBER);
+                try {
+                    Student student = new Student(name, Double.parseDouble(averageGrade), Integer.parseInt(recordBookNumber));
+                    System.out.println(count(student, studentList) + " elements were found for:");
+                    System.out.println(student);
+                } catch (NumberFormatException e) {
+                    System.out.println("Values cant be cast to expected formats");
+                }
+
                 processRequest(scanner, studentList);
                 return;
             case "5":
-                studentList.print();
+                System.out.println(studentList);
                 return;
             case "0":
                 System.out.println("\n\nexit");
@@ -97,7 +107,7 @@ public class Main {
                 return;
         }
 
-        throw new IllegalStateException("Wrong action code. Choose 1-4 or 0.");
+        throw new IllegalStateException("Wrong action code. Choose 1-5 or 0.");
     }
 
     /**
@@ -109,11 +119,13 @@ public class Main {
         // реализовать паттерн "Стратегия" по введённому коду стратегии, добавить валидацию этого кода.
         Student.Builder builder = new Student.Builder();
 
-        Student[] studentArray = new Student[4];
+        Student[] studentArray = new Student[6];
         studentArray[0] = builder.name("Sasha").averageGrade(0.97).recordBookNumber(100101).build();
         studentArray[1] = builder.name("Bogdan").averageGrade(0.99).recordBookNumber(888888).build();
-        studentArray[2] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414144).build();
+        studentArray[2] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414141).build();
         studentArray[3] = builder.name("Tagir").averageGrade(0.98).recordBookNumber(366663).build();
+        studentArray[4] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414144).build();
+        studentArray[5] = builder.name("Kirill").averageGrade(1.00).recordBookNumber(414144).build();
 
         // Пример заполнения коллекции из стрима:
         Arrays.stream(studentArray).forEach(studentList::add);
@@ -128,19 +140,44 @@ public class Main {
     }
 
     /**
-     * Многопоточный счётчик элементов
+     * Определяет количество вхождений конкретного студента в коллекции.
+     * Делит коллекцию на {@link #NUM_THREADS} частей и обходит их в параллельных потоках.
      *
-     * @param name             {@link Student#name} для поиска
-     * @param averageGrade     {@link  Student#averageGrade} для поиска
-     * @param recordBookNumber {@link Student#recordBookNumber} для поиска
-     * @param threadNumber     выделенное количество потоков
+     * @param student     студент, которого будем искать
+     * @param studentList лист в котором будем искать
+     * @return
      */
-    static void count(final String name,
-                      final String averageGrade,
-                      final String recordBookNumber,
-                      final int threadNumber) {
-        // реализовать счётчик
-        System.out.println("Посчитан элемент : " + name + ", " + averageGrade + ", " + recordBookNumber);
+    static int count(final Student student, final List<Student> studentList) {
+        ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
+        List<Future<Integer>> futures = new LinkedList<>();
+
+        int bucketSize = studentList.size() / NUM_THREADS;
+        for (int i = 0; i < NUM_THREADS; i++) {
+            final int leftBound = i * bucketSize;
+            final int rightBound = (i == NUM_THREADS - 1) ? studentList.size() : (i + 1) * bucketSize;
+            Callable<Integer> task = () -> {
+                int localCount = 0;
+                for (int j = leftBound; j < rightBound; j++) {
+                    if (studentList.get(j).equals(student)) {
+                        localCount++;
+                    }
+                }
+                return localCount;
+            };
+            futures.add(executor.submit(task));
+        }
+
+        int totalCount = 0;
+        for (Future<Integer> future : futures) {
+            try {
+                totalCount += future.get(); // Собираем результаты
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        executor.shutdown();
+
+        return totalCount;
     }
 
     /**
@@ -149,17 +186,47 @@ public class Main {
      * @param sortCode Код стратегии по которой будет проводиться сортировка.
      */
     static void sort(final String sortCode, final List<Student> studentList) {
-        // реализовать 4 режима режима. По каждому из полей + сортировка только чётных зачётных книжек.
-        var context = new SortingContext<Student>(getStrategyBySortCode(sortCode));
+        if (!handleSortCode(sortCode)) return;
+
+        var context = new SortingContext<>(getStrategyBySortCode(sortCode));
+
+        System.out.println("Unsorted list");
+        System.out.println(studentList);
+        System.out.println("-------------------------");
+
         context.sort(studentList);
+
+        System.out.println("Sorted list");
+        System.out.println(studentList);
     }
 
+    /**
+     * Получение стратегии сортировки по коду сортировки
+     *
+     * @param sortCode код сортировки
+     */
     static SortStrategy<Student> getStrategyBySortCode(String sortCode) {
         return switch (sortCode) {
             case "1" -> new StudentQuickSortByName();
             case "2" -> new StudentQuickSortByAverageGrade();
             case "3" -> new StudentQuickSortByRecordBookNumber();
             default -> new AdditionalStudentQuickSortByRecordBookNumber();
+        };
+    }
+
+    /**
+     * Проверка корректности введенного кода сортировки
+     *
+     * @param sortCode код стратегии сортировки
+     */
+    static boolean handleSortCode(String sortCode) {
+        return switch (sortCode) {
+            case "1", "2", "3", "4" -> true;
+            case "0" -> false;
+            default -> {
+                System.out.println("Wrong action code. Choose 1-4 or 0.");
+                yield false;
+            }
         };
     }
 }
